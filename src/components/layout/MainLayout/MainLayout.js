bb707c8a09cad949cf6d0d82aff0cb5e5f325c83
Justyna Żurawiecka <justyna2106@wp.pl>
2020-12-12 20:26:18 +0100

    Save valid viewport in state WIP

--- src/components/layout/MainLayout/MainLayout.js
+++ src/components/layout/MainLayout/MainLayout.js
@@ -1,18 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import Promoted from '../Promoted/Promoted';
 
-const MainLayout = ({ children }) => (
-  <div>
-    <Header />
-    <Promoted />
-    {children}
-    <Footer />
-  </div>
-);
+function useWindowWidth() {
+  const [width, setWidth] = useState([window.innerWidth]);
+  useEffect(() => {
+    const handleResize = () => {
+      setWidth([window.innerWidth]);
+    };
+    window.addEventListener('resize', handleResize);
+  }, []);
+  if (width <= 767) {
+    return width + 'px' + ' ' + 'Mobile';
+  } else if (768 <= width && width <= 1024) {
+    return width + 'px' + ' ' + 'Tablet';
+  } else if (width > 1025) {
+    return width + 'px' + ' ' + 'Desktop';
+  }
+}
+
+const MainLayout = ({ children }) => {
+  console.log(useWindowWidth());
+  return (
+    <div>
+      <Header />
+      <Promoted />
+      {children}
+      <Footer />
+    </div>
+  );
+};
 
 MainLayout.propTypes = {
   children: PropTypes.node,
